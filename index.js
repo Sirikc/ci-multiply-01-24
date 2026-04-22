d8955b7683f598daa8aa10b2bd4403f2b3aae550
sirikc <mishao090408@gmail.com>
2026-04-22 12:55:32 +0300

    4. add index.html

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const nodeBtn = document.querySelector("#btn");
 export const degree = (a, b)=>{
     if (b === 0) {
         return "infinit";
     }
     return a / b;
 }
+
+function buttonDegree(degree, inputId1, inputId2) {
+    const a = +document.getElementById(inputId1).value;
+    const b = +document.getElementById(inputId2).value;
+    console.log(degree(a, b));
+}
+
+nodeBtn.addEventListener("click", ()=>buttonDegree(degree, "input1", "input2"));
